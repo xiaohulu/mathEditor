c5aa5cbbe111aae365a797f34bbe3da2b794fbbb
xiaohulu <idocument@qq.com>
2018-01-05 07:06:57 +0800

    test prettier for ts files

--- tests/functional/MathEditor.ts
+++ tests/functional/MathEditor.ts
@@ -2,15 +2,14 @@ const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
 registerSuite('MathEditor functional tests', {
-    'run'() {
-        return this.remote
-                .get('index1.html')
-                .findById('mathEditor')
-                .clickMouseButton(0)
-                .type("a")
-                .then(() => {
-                    assert.isTrue(true);
-                });
-
-    }
-})
+	run() {
+		return this.remote
+			.get('index1.html')
+			.findById('mathEditor')
+			.clickMouseButton(0)
+			.type('a')
+			.then(() => {
+				assert.isTrue(true);
+			});
+	}
+});
